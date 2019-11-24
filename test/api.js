fed5feb887f7d7042408369f3593f031fa4cc0ed
Vladimir Dronnikov <dronnikov@gmail.com>
2019-11-24 09:59:37 +0300

    added unit test for custom level filter

--- test/api.js
+++ test/api.js
@@ -179,3 +179,50 @@ test('filters data using a level name', function (t) {
 
   lines.forEach(line => origin.write(JSON.stringify(line) + '\n'))
 })
+
+test('filters data using a custon level number', function (t) {
+  t.plan(9)
+
+  const lines = [{
+    level: 30,
+    msg: 'hello world'
+  }, {
+    level: 20,
+    msg: 'a debug statement'
+  }, {
+    level: 35,
+    msg: 'a custom level log line'
+  }, {
+    level: 40,
+    msg: 'a warning'
+  }]
+
+  const lines2 = Array.from(lines)
+  const lines30 = lines.filter(x => x.level >= 30)
+  const lines35 = lines.filter(x => x.level >= 35)
+
+  const teed30 = split(JSON.parse)
+  const teed35 = split(JSON.parse)
+  const dest = split(JSON.parse)
+  const origin = new PassThrough()
+
+  const instance = tee(origin)
+
+  instance.pipe(dest)
+  instance.tee(teed30, 30)
+  instance.tee(teed35, 35)
+
+  dest.on('data', function (d) {
+    t.deepEqual(d, lines2.shift())
+  })
+
+  teed30.on('data', function (d) {
+    t.deepEqual(d, lines30.shift())
+  })
+
+  teed35.on('data', function (d) {
+    t.deepEqual(d, lines35.shift())
+  })
+
+  lines.forEach(line => origin.write(JSON.stringify(line) + '\n'))
+})
